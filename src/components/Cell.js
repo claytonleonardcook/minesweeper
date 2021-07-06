9be981762a20517a9c7172cc609f37e4dc34a0ad
Clayton Cook <claytonleonardcook@gmail.com>
2021-07-06 14:05:22 -0400

    removed aspect ratio style

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -12,7 +12,8 @@ function Cell({ x, y, grid: [grid, setGrid], alive: [alive, setAlive] }) {
             style={{
                 animation: (!alive && data === 1) ? `shake 0.5s ease-in 0s forwards` : 'none',
                 backgroundColor: toggled ? '#FFF0' : '',
-                height: `${((window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight) - 100) / grid.length}px`
+                height: `${((window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight) - 100) / grid.length}px`,
+                width: `${((window.innerHeight > window.innerWidth ? window.innerWidth : window.innerHeight) - 100) / grid.length}px`
             }}
             onClick={() => {
                 if (!toggled) {
